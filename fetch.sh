4e731c472b98f3722b09a7597409bbcc53608bd3
Andrew Herbig <notandrewherbig@gmail.com>
2020-12-17 12:55:42 -0500

    move shit around, also scrape league data

---
+++ fetch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -euxo pipefail
+
+paths=(
+	api/general/stats
+	api/users/lists/league/all
+)
+
+
+for path in ${paths[@]}
+do
+	mkdir -p $(dirname $path)
+	curl -s https://ch.tetr.io/$path | jq .data > $path.json
+done
